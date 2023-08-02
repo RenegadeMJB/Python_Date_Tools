d2a64b5b145339d0ee995fecb2a46a60d3520f2b
RalstonScienceApps <MJBay86@gmail.com>
2023-08-02 10:56:15 -0500

    fixed the file structure, again

--- pythondatetools.py
+++
@@ -1,5 +0,0 @@
-def BaseDate():
-    print('The year 1970 started on a Thursday. It was not a leap year')
-    print('The year 1970 was chosen because it is the begining of the UNIX\n\
-          epoch. This seemed like a perfectly valid choice for these date\n\
-          tools')
